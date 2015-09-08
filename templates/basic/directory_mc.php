9ce6b6c078026488eb5333808f19b372965f8ffc
mshenhera <mykola.shenhera@gmail.com>
2015-09-08 09:53:57 +0000

    Added categories to search page.

--- templates/basic/directory_mc.php
+++ templates/basic/directory_mc.php
@@ -37,8 +37,9 @@
 <?php foreach($users_list['users'] as $user):
     $user_id = $user->ID;
     $display_name = $xoouserultra->userpanel->get_display_name($user_id);
-    $user_logo = $xoouserultra->userpanel->get_user_pic( $user_id, 125, $pic_type, $pic_boder_type, $pic_size_type);
+    $user_logo = $xoouserultra->userpanel->get_user_pic( $user_id, 125, $pic_type, 'rounded', $pic_size_type);
     $prof_category = wp_strip_all_tags(get_user_meta($user_id, 'prof_category', true), true);
+    $prof_category_array = explode(',', $prof_category);
     $region = wp_strip_all_tags(get_user_meta($user_id, 'region', true), true);
     $city = wp_strip_all_tags(get_user_meta($user_id, 'city', true), true);
     $phone = wp_strip_all_tags(get_user_meta($user_id, 'phone', true), true);
@@ -49,6 +50,19 @@
     $profile_url = $site_url."profile/".$user_id."/";
     $upload_folder =  $xoouserultra->get_option('media_uploading_folder');
     $rows = $xoouserultra->photogallery->get_user_photos($user_id, $display_latest_photos_howmany);
+    $prof_category_0 = wp_strip_all_tags(get_user_meta($user_id, 'prof_category_0', true), true);
+    $prof_category_array_0 = explode(',', $prof_category_0);
+    $prof_category_1 = wp_strip_all_tags(get_user_meta($user_id, 'prof_category_1', true), true);
+    $prof_category_array_1 = explode(',', $prof_category_1);
+    $prof_category_2 = wp_strip_all_tags(get_user_meta($user_id, 'prof_category_2', true), true);
+    $prof_category_array_2 = explode(',', $prof_category_2);
+    $prof_category_3 = wp_strip_all_tags(get_user_meta($user_id, 'prof_category_3', true), true);
+    $prof_category_array_3 = explode(',', $prof_category_3);
+    $prof_category_4 = wp_strip_all_tags(get_user_meta($user_id, 'prof_category_4', true), true);
+    $prof_category_array_4 = explode(',', $prof_category_4);
+    $prof_category_5 = wp_strip_all_tags(get_user_meta($user_id, 'prof_category_5', true), true);
+    $prof_category_array_5 = explode(',', $prof_category_5);
+    $prof_category_array_x = array_merge($prof_category_array_0, $prof_category_array_1, $prof_category_array_2, $prof_category_array_3, $prof_category_array_4, $prof_category_array_5);
 ?>
 <div class="search-box">
     <div class="row row-main">
@@ -73,16 +87,32 @@
                     </div>
                 </div>
                 <div class="w-002col">
-                    <div class="font-category box-body padding-profile">
+                    <div class="font-category box-body box-profile padding-profile">
                         <ul class="category-list">
-                            <li>Profile 11</li><li>Profile 12</li><li>Profile 13</li><li>Profile 14</li>
+                        <?php $index = 0;
+                            foreach ($prof_category_array as $category):
+                                $index = $index + 1;
+                                if ($category and $index == 3): ?>
+                            <li><?php echo $category ?> ...</li>
+                                <?php break; elseif ($category): ?>
+                            <li><?php echo $category ?></li>
+                        <?php endif;
+                            endforeach ?>
                         </ul>
                     </div>
                 </div>
                 <div class="w-002col">
-                    <div class="font-category box-body padding-profile">
+                    <div class="font-category box-body box-profile padding-profile-2">
                         <ul class="category-list">
-                            <li>Profile 21</li><li>Profile 22</li><li>Profile 23</li>
+                        <?php $index = 0;
+                            foreach ($prof_category_array_x as $sub_category):
+                                $index = $index + 1;
+                                if ($sub_category and $index == 3): ?>
+                            <li class="font-category"><?php echo $sub_category ?> ...</li>
+                                <?php break; elseif ($sub_category): ?>
+                            <li class="font-category"><?php echo $sub_category ?></li>
+                        <?php endif;
+                            endforeach ?>
                         </ul>
                     </div>
                 </div>
